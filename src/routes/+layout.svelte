9b060b293270936c2026d725f82604c34255784c
bruol <lorin@urbantat.eu>
2026-04-21 12:12:44 +0200

    switch to plausible npm package

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	import './layout.css';
 
 	let { children } = $props();
+
+	onMount(async () => {
+		const { init } = await import('@plausible-analytics/tracker');
+
+		init({
+			domain: 'co-werk5.ch',
+			endpoint: 'https://plausible.bruol.me/api/event'
+		});
+	});
 </script>
 
 {@render children()}
